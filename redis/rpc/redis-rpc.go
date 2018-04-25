791b046aaa2dc65b2a7f60d0c4d1f455fc074914
洪涛 <ht@xkeshi.com>
2018-04-25 17:49:23 +0800

    #Bugfix

--- redis/rpc/redis-rpc.go
+++ redis/rpc/redis-rpc.go
@@ -5,9 +5,10 @@ import (
 	"net"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"github.com/lampard1014/aphro/redis/pb"
+	pb "github.com/lampard1014/aphro/redis/pb"
     "fmt"
-    "github.com/go-redis/redis"
+    "time"
+    r "github.com/go-redis/redis"
 
 )
 
@@ -17,68 +18,77 @@ const (
 
 type redisService struct{}
 
-func (s *redisService) Query(ctx context.Context, in *QueryRedisRequest) (*QueryRedisResponse, error) {
+func (s *redisService) Query(ctx context.Context, in *pb.QueryRedisRequest) (*pb.QueryRedisResponse, error) {
     cli := createClient()
     val, err := cli.Get(in.Key).Result()
-    if err != nil {
+    fmt.Println("getVal",val)
+    if val != "" && err != nil {
         panic(err)
     }
-    return &QueryRedisResponse{
+    return &pb.QueryRedisResponse{
         Successed:err == nil,
         Value:val,
     }, err
 }
 
-func (s *redisService) IsExists(ctx context.Context, in *IsExistsRequest) (*IsExistsResponse, error) {
+func (s *redisService) IsExists(ctx context.Context, in *pb.IsExistsRequest) (*pb.IsExistsResponse, error) {
     cli := createClient()
     isSuccess,err := cli.Exists(in.Key).Result()
+
+    bingo := isSuccess == 1
+    // fmt.Println("isExists:",in.Key,isSuccess,err,bingo)
     if err != nil {
         panic(err)
     }
-    return &IsExistsResponse{IsExists:isSuccess == 1},err
+    return &pb.IsExistsResponse{IsExists:bingo},err
 }
 
-func (s *redisService) ExpireAt(ctx context.Context, in *ExpireAtRequest) (*ExpireAtResponse, error) {
+func (s *redisService) ExpireAt(ctx context.Context, in *pb.ExpireAtRequest) (*pb.ExpireAtResponse, error) {
     cli := createClient()
-    isSuccess,err := cli.ExpireAt(in.Key,time.Unix(in.Ttl,0)).Result()
+    isSuccess,err := cli.ExpireAt(in.Key,time.Unix(int64(in.Ttl),0)).Result()
     if err != nil {
         panic(err)
     }
-    return &IsExistsResponse{Successed:isSuccess},err
+    return &pb.ExpireAtResponse{Successed:isSuccess},err
 }
 
+func (s *redisService) QueryTtl(ctx context.Context, in *pb.QueryTtlRedisRequest) (*pb.QueryTtlRedisResponse, error) {
+    cli := createClient()
+    ttl,err := cli.TTL(in.Key,).Result()
+    fmt.Println("err ttl",err)
+    if err != nil {
+        panic(err)
+    }
+    return &pb.QueryTtlRedisResponse{Ttl:int64(ttl.Seconds())},err
+}
 
-func (s *redisService) Delete(ctx context.Context, in *DeleteRedisRequest) (*DeleteRedisResponse, error) {
+func (s *redisService) Delete(ctx context.Context, in *pb.DeleteRedisRequest) (*pb.DeleteRedisResponse, error) {
     cli := createClient()
     isSuccess,err := cli.Del(in.Key).Result()
     if err != nil {
         panic(err)
     }
-    return &DeleteRedisResponse{Successed:isSuccess == 1},err
+    return &pb.DeleteRedisResponse{Successed:isSuccess == 1},err
 }
 
-func (s *redisService) Set(ctx context.Context, in *SetRedisRequest) (*SetRedisResponse, error) {
+func (s *redisService) Set(ctx context.Context, in *pb.SetRedisRequest) (*pb.SetRedisResponse, error) {
     cli := createClient()
-    isSuccess,err := cli.Set(in.Key,in.Value,in.Ttl).Result()
+    _,err := cli.Set(in.Key,in.Value,time.Duration(in.Ttl)).Result()
     if err != nil {
         panic(err)
     }
-    return &SetRedisResponse{Successed:err == nil},err
+    return &pb.SetRedisResponse{Successed:err == nil},err
 }
 
 
-var redisCli : *Client = nil
-
-func createClient()(*Client) {
-    if redisCli == nil {
-        redisCli := redis.NewClient(&redis.Options{
-            Addr:     "127.0.0.1:6379",
-            Password: "", // no password set
-            DB:       0,  // use default DB
-        })
-        pong, err := client.Ping().Result()
-        fmt.Println(pong, err)
-    }
+func createClient()*r.Client {
+    redisCli := r.NewClient(&r.Options{
+        Addr:     "127.0.0.1:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+    pong, err := redisCli.Ping().Result()
+    fmt.Println(pong, err,redisCli)
     return redisCli
 }
 
@@ -98,7 +108,7 @@ func main() {
     }
 
     s := grpc.NewServer()
-    RegisterRedisServiceServer(s, new(redisService))
+    pb.RegisterRedisServiceServer(s, new(redisService))
     err = s.Serve(lis)
     if err != nil {
         log.Fatal(err)
--- session/rpc/session-rpc.go
+++ session/rpc/session-rpc.go
@@ -3,120 +3,216 @@ package  main
 import (
 	"log"
 	"net"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-    "github.com/xxtea/xxtea-go/xxtea"
-	"github.com/lampard1014/aphro/session/pb"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    // "github.com/xxtea/xxtea-go/xxtea"
+	sessionPb "github.com/lampard1014/aphro/session/pb"
+    redisPb "github.com/lampard1014/aphro/redis/pb"
     "fmt"
+    "time"
+    "math/rand"
+
 )
 
 const (
 	port  = ":10088"
+    redisRpcAddress = "127.0.0.1:10101"
+    tokenDuration = 24 * 3600 * time.Second //1 day
 )
 
 type sessionService struct{}
 
-func (s *sessionService) QuerySessionToken(ctx context.Context, in *SessionTokenQueryRequest) (*SessionTokenQueryResponse, error) {
-}
-
-func (s *sessionService) CreateSessionToken(ctx context.Context, in *SessionTokenCreateRequest) (*SessionTokenCreateResponse, error) {
-}
-
-func (s *sessionService) DeleteSessionToken(ctx context.Context, in *DeleteSessionTokenRequest) (*DeleteSessionTokenResponse, error) {
-}
-
-func (s *sessionService) RenewSessionToken(ctx context.Context, in *RenewSessionTokenRequest) (*RenewSessionTokenResponse, error) {
-}
-
-func (s *sessionService) IsSessionTokenVailate(ctx context.Context, in *IsSessionTokenVailateRequest) (*IsSessionTokenVailateResponse, error) {
-}
-
-func (s *sessionService) MerchantVerifyCode(ctx context.Context, in *MerchantVerifyCodeRequest) (*MerchantVerifyCodeResponse, error) {
-}
-
-func (s *sessionService) MerchantSendCode(ctx context.Context, in *MerchantSendCodeRequest) (*MerchantSendCodeResponse, error) {
-}
-
-
-func (s *encryptionService) Base64Encode(ctx context.Context, in *pb.EncryptionBase64EncodeRequest) (*pb.EncryptionBase64EncodeResponse, error) {
-    str := base64.StdEncoding.EncodeToString(in.RawValue)
-    return &pb.EncryptionBase64EncodeResponse{EncodedStr:str},nil
-}
-
-func (s *encryptionService) Base64Decode(ctx context.Context, in *pb.EncryptionBase64DecodeRequest) (*pb.EncryptionBase64DecodeResponse, error) {
-    decodeBytes , _ := base64.StdEncoding.DecodeString(in.DecodedStr)
-    return &pb.EncryptionBase64DecodeResponse{RawValue:decodeBytes},nil
-}
-
-func (s *encryptionService) XxteaEncryption(ctx context.Context, in *pb.EncryptionXXTEARequest) (*pb.EncryptionXXTEAResponse, error) {
-    encrypt_data := xxtea.Encrypt([]byte(in.RawValue),[]byte(in.Key))
-    return &pb.EncryptionXXTEAResponse{Key:in.Key,EncryptedStr:encrypt_data},nil
-}
-
-func (s *encryptionService) XxteaDecryption(ctx context.Context, in *pb.DecryptionXXTEARequest) (*pb.DecryptionXXTEAResponse, error) {
-    decrypt_data := xxtea.Decrypt([]byte(in.EncryptedStr),[]byte(in.Key))
-    return &pb.DecryptionXXTEAResponse{Key:in.Key,RawValue:string(decrypt_data)},nil
-}
-
-func (s *encryptionService) RsaEncryption(ctx context.Context, in *pb.EncryptionRSARequest) (*pb.EncryptionRSAResponse, error) {
-   encrypedData := RsaEncrypt(in.RawValue)
-   return &pb.EncryptionRSAResponse{EncryptedStr:encrypedData},nil;
-}
-
-func (s *encryptionService) RsaDecryption(ctx context.Context, in *pb.DecryptionRSARequest) (*pb.DecryptionRSAResponse, error) {
-    decryptdData := RsaDecrypt(in.EncryptedStr)
-    return &pb.DecryptionRSAResponse{RawValue:decryptdData},nil
-}
-
-var pemMap = map[string]string{"public": "../rsa/public.pem", "private": "../rsa/private.pem"}
-
-func GetBlockFromPem(key string) []byte {
-    path := pemMap[key]
-    fi, err := os.Open(path)
+func (s *sessionService) QuerySessionToken(ctx context.Context, in *sessionPb.SessionTokenQueryRequest) (*sessionPb.SessionTokenQueryResponse, error) {
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
     if err != nil {
         panic(err)
     }
-    defer fi.Close()
-    fd, err := ioutil.ReadAll(fi) //读取文件内容
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    queryRes, err1 := c.Query(ctx, &redisPb.QueryRedisRequest{Key: in.SessionToken})
+    QueryTtlRes,err := c.QueryTtl(ctx, &redisPb.QueryTtlRedisRequest{Key:in.SessionToken})
 
-     pemKey := []byte(string(fd))
-    block, _ := pem.Decode(pemKey)
-    if block == nil {
-        panic(key + " key error!")
+    hasError := err != nil || err1 != nil
+    sessionValue := ""
+    if queryRes!= nil  {
+            fmt.Println("bingo",queryRes.Value)
+        sessionValue = queryRes.Value
     }
-    return block.Bytes
+    if QueryTtlRes.Ttl >= 0 &&  hasError{
+        panic(err)
+    }
+
+
+    fmt.Println(sessionValue)
+
+    return &sessionPb.SessionTokenQueryResponse{
+            SessionToken:sessionValue,
+            Ttl:QueryTtlRes.Ttl,
+            Successed:!hasError,
+        },err
 }
 
-// 加密
-func RsaEncrypt(origData []byte) []byte {
-    publicPem := GetBlockFromPem("public") //获取公钥pem的block
-    pubInterface, err := x509.ParsePKIXPublicKey(publicPem) //解析公钥
+func (s *sessionService) CreateSessionToken(ctx context.Context, in *sessionPb.SessionTokenCreateRequest) (*sessionPb.SessionTokenCreateResponse, error) {
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
     if err != nil {
         panic(err)
     }
-    pub := pubInterface.(*rsa.PublicKey)
-    encypt, err := rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    token := GetRandomString(6)
+
+    setRes, err := c.Set(ctx, &redisPb.SetRedisRequest{Key:token,Value:in.SessionTokenRequestStr,Ttl:uint64(tokenDuration)})
+
     if err != nil {
         panic(err)
     }
-    return encypt
+
+    queryRes, err2 := c.Query(ctx, &redisPb.QueryRedisRequest{Key: token})
+
+    fmt.Println(token,in.SessionTokenRequestStr,err,time.Now(),int64(tokenDuration))
+
+    fmt.Println(queryRes,err2)
+
+    return &sessionPb.SessionTokenCreateResponse{
+            SessionToken:token,
+            Ttl:int64(tokenDuration),
+            Successed:setRes.Successed,
+        },err
 }
 
-// 解密
-func RsaDecrypt(encypt []byte) []byte {
-    privatePem := GetBlockFromPem("private")
-    priv, err := x509.ParsePKCS1PrivateKey(privatePem) //解析私钥
-    if err != nil {
-    panic(err)
-    }
-    decypt, err := rsa.DecryptPKCS1v15(rand.Reader, priv, encypt)
-
-    if err != nil {
-    panic(err)
-    }
-    return decypt
+func  GetRandomString(l int) string {  
+    str := "0123456789abcdefghijklmnopqrstuvwxyz"  
+    bytes := []byte(str)  
+    result := []byte{}  
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))  
+    for i := 0; i < l; i++ {  
+        result = append(result, bytes[r.Intn(len(bytes))])  
+    }  
+    return string(result)  
 }
 
+
+func (s *sessionService) DeleteSessionToken(ctx context.Context, in *sessionPb.DeleteSessionTokenRequest) (*sessionPb.DeleteSessionTokenResponse, error) {
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    // ttl := time.Now().Add(tokenDuration)
+    delRes, err := c.Delete(ctx, &redisPb.DeleteRedisRequest{Key:in.SessionToken})
+    if err != nil {
+        panic(err)
+    }
+    return &sessionPb.DeleteSessionTokenResponse{
+            Successed:delRes.Successed,
+        },err
+
+}
+
+func (s *sessionService) RenewSessionToken(ctx context.Context, in *sessionPb.RenewSessionTokenRequest) (*sessionPb.RenewSessionTokenResponse, error) {
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    ttl := int64(time.Now().Add(tokenDuration).Unix())
+    token := in.SessionToken
+    res, err := c.ExpireAt(ctx, &redisPb.ExpireAtRequest{Key:token,Ttl:ttl})
+    if err != nil {
+        panic(err)
+    }
+    return &sessionPb.RenewSessionTokenResponse{
+            Ttl:ttl,
+            Successed:res.Successed,
+        },err
+}
+
+func (s *sessionService) IsSessionTokenVailate(ctx context.Context, in *sessionPb.IsSessionTokenVailateRequest) (*sessionPb.IsSessionTokenVailateResponse, error) {
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    token := in.SessionToken
+    res, err := c.IsExists(ctx, &redisPb.IsExistsRequest{Key:token})
+    fmt.Println("res ", res,err)
+    if err != nil {
+        panic(err)
+    }
+    return &sessionPb.IsSessionTokenVailateResponse{
+            Successed:res.IsExists,
+        },err 
+}
+
+func (s *sessionService) MerchantVerifyCode(ctx context.Context, in *sessionPb.MerchantVerifyCodeRequest) (*sessionPb.MerchantVerifyCodeResponse, error) {
+
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+
+    cellphone :=  in.Cellphone
+    scence :=  fmt.Sprint(in.Scence)
+    smsCode := in.SmsCode
+    checkKey := "_verify_sms_"+ cellphone + "@" + scence
+    queryRes, err := c.Query(ctx, &redisPb.QueryRedisRequest{Key: checkKey})
+
+    fmt.Println("xxx",queryRes, err)
+
+    if err != nil {
+        panic(err)
+    }
+
+    return &sessionPb.MerchantVerifyCodeResponse{
+             Successed:queryRes.Value == smsCode,
+        },err
+}
+
+func (s *sessionService) MerchantSendCode(ctx context.Context, in *sessionPb.MerchantSendCodeRequest) (*sessionPb.MerchantSendCodeResponse, error) {
+
+    conn,err := grpc.Dial(redisRpcAddress,grpc.WithInsecure())
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+    c := redisPb.NewRedisServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+
+    cellphone :=  in.Cellphone
+    scence :=  fmt.Sprint(in.Scence)
+
+    // token := in.Token
+    checkKey := "_verify_sms_"+ cellphone + "@" + scence
+    smsCodeTmp := "123456"
+    ttl := uint64(60 * time.Second)//60秒过期
+
+    setRes, err := c.Set(ctx, &redisPb.SetRedisRequest{Key:checkKey,Value:smsCodeTmp,Ttl:ttl})
+    if err != nil {
+        panic(err)
+    }
+
+    return &sessionPb.MerchantSendCodeResponse{
+             Successed:setRes.Successed,
+        },err
+}
 
 func deferFunc() {
     if err := recover(); err != nil {
@@ -133,7 +229,7 @@ func main() {
     }
 
     s := grpc.NewServer()
-    RegisterSessionServiceServer(s, new(sessionService))
+    sessionPb.RegisterSessionServiceServer(s, new(sessionService))
     err = s.Serve(lis)
     if err != nil {
         log.Fatal(err)
