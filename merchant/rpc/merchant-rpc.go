170e2c2f7accdf24bce1a97c44d2d29b6dfe477a
洪涛 <ht@xkeshi.com>
2018-04-27 12:05:31 +0800

    #Update bugfix

--- merchant/rpc/merchant-rpc.go
+++ merchant/rpc/merchant-rpc.go
@@ -10,6 +10,8 @@ import (
     "strings"
     "database/sql"
     "errors"
+    "crypto/sha256"
+    "encoding/base64"
     _ "github.com/go-sql-driver/mysql"
     merchantServicePB "github.com/lampard1014/aphro/merchant/pb"
     encryptionServicePB "github.com/lampard1014/aphro/encryption/encryption-pb"
@@ -27,6 +29,40 @@ const (
 
 type merchantService struct{}
 
+
+func pswEncryption(psw string) (encryptionPsw string) {
+    h := sha256.New()
+    h.write([]byte(psw))
+    encryptionPsw := base64.URLEncoding.EncodeToString(h.Sum(nil))
+    return encryptionPsw
+}
+
+func fetchSessionTokenValue(sessionToken string) (uid, merchantID, err) {
+
+    var returnErr error = nil
+    sessionRPCConn,sessionRPCErr := grpc.Dial(sessionRPCAddress,grpc.WithInsecure())
+    if sessionRPCErr == nil {
+        sessionRPCConnClient := sessionServicePB.NewSessionServiceClient(sessionRPCConn)
+
+        sessionRPCConnClientCtx, sessionRPCConnClientCancel := context.WithTimeout(context.Background(), time.Second)
+        defer sessionRPCConnClientCancel()
+        sessionTokenQueryResponse, sessionTokenQueryResponseErr := sessionRPCConnClient.querySessionToken(sessionRPCConnClientCtx, &sessionServicePB.SessionTokenQueryRequest{SessionToken:sessionToken})
+        if sessionTokenQueryResponseErr == nil && sessionTokenQueryResponse != nil {
+            sessionTokenValue := sessionTokenQueryResponse.SessionToken
+            splitValue := strings.Split(sessionTokenValue, "#")
+            uidAndMerchantID := strings.Split(splitValue[0],"@")
+            uid = uidAndMerchantID[0]
+            merchantID = uidAndMerchantID[1]
+        } else {
+            returnErr = errors.New("session expired, pls login again")
+        }
+    } else {
+        returnErr = sessionRPCErr
+    }
+    defer sessionRPCConn.Close()
+    return uid,merchantID,returnErr
+}
+
 func parseUsernameAndPsw(key string)(username string ,psw string, err error) {
 
     fmt.Println("parseUsernameAndPsw")
@@ -40,10 +76,8 @@ func parseUsernameAndPsw(key string)(username string ,psw string, err error) {
         defer cancel()
         base64DecodeRes, base64DecodeErr := c.Base64Decode(ctx,&encryptionServicePB.EncryptionBase64DecodeRequest{DecodedStr:key})
         if base64DecodeErr == nil {
-
             rawData, RSADecryptionErr := c.RsaDecryption(ctx, &encryptionServicePB.DecryptionRSARequest{EncryptedStr:base64DecodeRes.RawValue})
             if RSADecryptionErr == nil {
-
                 usernameAndPsw := string(rawData.RawValue)
                 tmpSplit := strings.Split(usernameAndPsw,"@|@")
                 if 2 == len(tmpSplit) {
@@ -54,7 +88,7 @@ func parseUsernameAndPsw(key string)(username string ,psw string, err error) {
                     returnErr := errors.New("拆分用户名密码错误")
                 }
             } else {
-               returnErr = base64DecodeErr
+               returnErr = RSADecryptionErr
             }
         } else {
             returnErr = base64DecodeErr
@@ -132,7 +166,7 @@ func (s *merchantService) MerchantRegister(ctx context.Context, in *merchantServ
                 stmtIns, stmtInsErr := db.Prepare("INSERT INTO `merchant_account` (`name`,`cellphone`,`psw`,`role`,`merchant_id`)VALUES(?,?,?,?,?)") // ? = placeholder
                 if stmtInsErr == nil {
 
-                    insertResult, insertErr := stmtIns.Exec(name, cellphone, psw, role,merchantID) 
+                    insertResult, insertErr := stmtIns.Exec(name, cellphone, pswEncryption(psw), role,merchantID) 
 
                     if insertErr == nil {
                         afftectedRow, afftectedRowErr := insertResult.RowsAffected()
@@ -164,7 +198,100 @@ func (s *merchantService) MerchantRegister(ctx context.Context, in *merchantServ
 }
 
 func (s *merchantService) MerchantChangePsw(ctx context.Context, in *merchantServicePB.MerchantChangePswRequest) (*merchantServicePB.MerchantChangePswResponse, error) {
-    return nil,nil
+
+    newPsw := in.NewPsw
+    sessionToken := in.SessionToken
+    verifyCode := in.VerifyCode
+    cellphone := in.Cellphone
+    scene := 2
+    var operatorSuccess bool = false
+    var returnErr error = nil
+    //检查session 是否合法
+
+    sessionRPCConn,sessionRPCErr := grpc.Dial(sessionRPCAddress,grpc.WithInsecure())
+    if sessionRPCErr == nil {
+        sessionRPCConnClient := sessionServicePB.NewSessionServiceClient(sessionRPCConn)
+
+        sessionRPCConnClientCtx, sessionRPCConnClientCancel := context.WithTimeout(context.Background(), time.Second)
+
+        defer sessionRPCConnClientCancel()
+        var isSessionTokenOK bool = false
+        isSessionTokenVailate, isSessionTokenVailateErr := sessionRPCConnClient.IsSessionTokenVailate(sessionRPCConnClientCtx, &sessionServicePB.IsSessionTokenVailateRequest{SessionToken:sessionToken})
+        if isSessionTokenVailateErr == nil {
+            isSessionTokenOK := isSessionTokenVailate.Successed
+
+            if isSessionTokenOK {
+                encyptRPCConn,encyptRPCErr := grpc.Dial(encyptRPCAddress,grpc.WithInsecure())
+                var operatorSuccess bool = false
+                var returnErr error = nil
+                if encyptRPCErr == nil {
+                    encyptRPCClient := encryptionServicePB.NewEncryptionServiceClient(encyptRPCConn)
+                    encyptRPCClientCtx, encyptRPCClientCancel := context.WithTimeout(context.Background(), time.Second)
+                    defer encyptRPCClientCancel()
+                    base64DecodeRes, base64DecodeErr := c.Base64Decode(ctx,&encryptionServicePB.EncryptionBase64DecodeRequest{DecodedStr:newPsw})
+                    if base64DecodeErr == nil {
+                        rawData, RSADecryptionErr := c.RsaDecryption(ctx, &encryptionServicePB.DecryptionRSARequest{EncryptedStr:base64DecodeRes.RawValue})
+                        if RSADecryptionErr == nil {
+                            newPsw := string(rawData.RawValue)
+                            if nil != newPsw {
+                                    //获取到最新的密码 验证 验证码是否正常
+                                    verifyCodeRes ,verifyCodeResErr := sessionRPCConnClient.MerchantVerifyCode(sessionRPCConnClientCtx,&sessionServicePB.MerchantVerifyCodeRequest{Cellphone:cellphone,Scene:scene,SmsCode:verifyCode})
+                                    if verifyCodeRes.Successed {
+                                        db, dbOpenErr := sql.Open("mysql", mysqlDSN)
+                                        defer db.Close()
+                                        dbOpenErr = db.Ping()
+                                        if dbOpenErr == nil {
+                                            stmtIns, stmtInsErr := db.Prepare("update `merchant_account` set psw = ? where cellphone = ? limit 1") // ? = placeholder
+                                            if stmtInsErr == nil {
+                                                updateResult, updateErr := stmtIns.Exec(pswEncryption(newPsw),cellphone) 
+                                                if updateErr == nil {
+                                                    afftectedRow, afftectedRowErr := insertResult.RowsAffected()
+                                                    if afftectedRow != 1 || afftectedRowErr == nil {
+                                                        returnErr := afftectedRowErr
+                                                    } else {
+                                                        //成功
+                                                        operatorSuccess = true
+                                                    }
+                                                } else {
+                                                    returnErr = insertErr
+                                                }
+                                            } else {
+
+                                                returnErr = stmtInsErr
+                                            }
+                                            defer stmtIns.Close()
+                                        } else {
+                                            returnErr = dbOpenErr
+                                        }   
+                                    } else {
+                                        returnErr = verifyCodeResErr
+                                    }
+                            } else {
+                                returnErr := errors.New("解析密码错误")
+                            }
+                        } else {
+                           returnErr = RSADecryptionErr
+                        }
+                    } else {
+                        returnErr = base64DecodeErr
+                    }
+                } else {
+                    returnErr = encyptRPCErr
+                }
+                defer encyptRPCConn.Close()
+
+            } else {
+                returnErr = errors.New("session expired, pls login again")
+            }
+        } else {
+            returnErr = isSessionTokenVailateErr
+        }
+    } else {
+        returnErr = sessionRPCErr
+    }
+    defer sessionRPCConn.Close()
+
+    return &merchantServicePB.MerchantChangePswResponse{Successed:operatorSuccess},returnErr
 }
 
 func (s *merchantService) MerchantLogin(ctx context.Context, in *merchantServicePB.MerchantLoginRequest) (*merchantServicePB.MerchantLoginResponse, error) {
@@ -220,7 +347,38 @@ func (s *merchantService) MerchantLogin(ctx context.Context, in *merchantService
 }
 
 func (s *merchantService) MerchantInfo(ctx context.Context, in *merchantServicePB.MerchantInfoRequest) (*merchantServicePB.MerchantInfoResponse, error) {
-    return nil,nil
+
+    //验证token合法性
+    uid, merchantID, sessionTokenError := fetchSessionTokenValue(in.SessionToken)
+    var returnErr error = nil
+    if sessionTokenError == nil {
+        var (
+            merchantName string
+            name string
+            role int
+        )
+        db, dbOpenErr := sql.Open("mysql", mysqlDSN)
+        defer db.Close()
+        dbOpenErr = db.Ping()
+        if dbOpenErr == nil {
+            queryRowErr := db.QueryRow("SELECT m.merchant_name,ma.role,ma.name FROM merchant_account AS ma, merchant AS m WHERE ma.merchant_id = ? and ma.ID = ? limit 1;", merchantID, uid).Scan(&merchantName,,&role,&name)
+            if queryRowErr == nil {
+                //制作 令牌
+            } else if queryRowErr == sql.ErrNoRows {
+                //没有记录
+                returnErr = errors.New("没用商户信息")
+            } else {
+                returnErr = queryRowErr
+            }
+
+        } else {
+            returnErr = dbOpenErr
+        }
+    } else {
+        returnErr = sessionTokenError
+    }
+
+    return &merchantServicePB.MerchantInfoResponse{MerchantName:merchantName,&merchantServicePB.InnerMerchantAccount{Role:uint32(role),Name:name}},returnErr
 }
 
 func deferFunc() {
